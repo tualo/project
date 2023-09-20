0f0464909b0101865ac22ede77e84fc9b67a71aa
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-09-20 09:52:38 +0200

    add cms route

---
+++ src/Routes/CMSReportPDF.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tualo\Office\Project\Routes;
+
+use Tualo\Office\Mail\OutgoingMail;
+use Tualo\Office\Basic\TualoApplication as App;
+use Tualo\Office\Basic\Route as BasicRoute;
+use Tualo\Office\Basic\IRoute;
+use Tualo\Office\DS\DSTable;
+use Tualo\Office\PUG\PUG;
+use Tualo\Office\RemoteBrowser\RemotePDF;
+use DOMDocument;
+use PHPMailer\PHPMailer\PHPMailer;
+
+
+class CMSReportPDF implements IRoute
+{
+    public static function register()
+    {
+
+        BasicRoute::add('/tualocms/page/report/(?P<type>[\w\-\_]+)/(?P<id>[\w\-\_]+)', function ($matches) {
+
+
+            $db = App::get('session')->getDB();
+            $session = App::get('session');
+            $types=[];
+            $types['bill'] = 'view_blg_list_rechnung';
+            $types['offer'] = 'view_blg_list_angebot';
+
+            try {
+                $project = $db->singleRow('select * from projectmanagement where project_id = {project_id} ', [
+                    'project_id' => $matches['id']
+                ]);
+                if (is_null($project)) throw new \Exception('Project not found');
+                if (!in_array($matches['type'],array_keys($types))) throw new \Exception('Type not allowed');
+
+                $template = $db->singleValue('select template from ds_renderer where table_name = {table_name} ', [
+                    'table_name' => $types[$matches['type']]
+                ]);
+                if ($template === false) throw new \Exception('Template not found');
+
+                $res=RemotePDF::get($types[$matches['type']],$template, $matches['id'], true);
+                App::contenttype('application/pdf');
+                readfile($res['localfilename']);
+                unlink($res['localfilename']);
+                
+                BasicRoute::$finished=true;
+            } catch (\Exception $e) {
+                App::result('msg', $e->getMessage());
+            }
+        }, ['get'], true);
+    }
+}
--- src/functions.php
+++ src/functions.php
@@ -2,3 +2,5 @@
 
 require_once "Compiler.php";
 require_once "Routes/ConvertOffer.php";
+require_once "Routes/CMSReportPDF.php";
+
