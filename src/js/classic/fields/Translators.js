a7fe70fdd09ec7e1208cac70bbf8f538810a6ef1
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-12-11 15:56:17 +0100

    debugs

--- src/js/classic/fields/Translators.js
+++ src/js/classic/fields/Translators.js
@@ -10,8 +10,16 @@ Ext.define('Tualo.project.form.field.Translators', {
     initComponent: function() {
         var fld = this;
         console.log('initComponent','tualo_projectmanagement_translators',fld);
+        if (fld.column) console.log('initComponent','tualo_projectmanagement_translators','column',fld.column);
+        if (fld.column.view) console.log('initComponent','tualo_projectmanagement_translators','view',fld.column.view);
+        if (fld.column.view.grid) console.log('initComponent','tualo_projectmanagement_translators','grid',fld.column.view.grid);
+
         setTimeout(()=>{
-            fld.view.grid.on('edit',function(editor, e){
+            if (fld.column) console.log('initComponent','tualo_projectmanagement_translators','column',fld.column);
+            if (fld.column.view) console.log('initComponent','tualo_projectmanagement_translators','view',fld.column.view);
+            if (fld.column.view.grid) console.log('initComponent','tualo_projectmanagement_translators','grid',fld.column.view.grid);
+
+            fld.column.view.grid.on('edit',function(editor, e){
                 console.log('edit',editor,e);
                 // fld.view.grid.getStore().sync();
             });
