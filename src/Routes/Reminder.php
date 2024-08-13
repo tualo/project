6473d0273cdcef59a70e93a2e0a75fb5249bdd19
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2024-08-13 13:34:18 +0200

    fix reminder

--- src/Routes/Reminder.php
+++ src/Routes/Reminder.php
@@ -24,10 +24,10 @@ class Reminder implements IRoute{
 
                 foreach($list as $reminderRow){
                     $table = DSTable::instance('projectmanagement');
-                    $project = $table->read()->f('project_id','=',$reminderRow['project_id'])->getSingle();
-                    $translator = \Tualo\Office\DS\DSTable::instance('uebersetzer')->f('kundennummer','=',$project['uebersetzer'])->read()->getSingle();
+                    $project = $table->f('project_id','=',$reminderRow['project_id'])->read()->getSingle();
+                    $translator = DSTable::instance('uebersetzer')->f('kundennummer','=',$project['uebersetzer'])->read()->getSingle();
                     $mission = [];
-                    $projectmanagement_mission =\Tualo\Office\DS\DSTable::instance('projectmanagement_mission');
+                    $projectmanagement_mission = DSTable::instance('projectmanagement_mission');
                     $table = $projectmanagement_mission->f('project_id','=',$project['project_id'])->read();
                     if (!$table->empty()){
                         $mission = $table->getSingle();
